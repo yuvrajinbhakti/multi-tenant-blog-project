9d90f71690c725d58f449b17a93a97e348117336
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2025-06-27 21:11:07 +0530

    Add database schema for blogs and new script for Drizzle ORM
    - Added a new script "db:push" to package.json for pushing database changes.
    - Created blogTable schema in schema.ts with fields for id, title, body, orgId, createdAt, and updatedAt.

---
+++ db/schema.ts
@@ -0,0 +1,19 @@
+import { pgTable, uuid, varchar, text, timestamp } from "drizzle-orm/pg-core";
+
+export const blogTable = pgTable('blogs',{
+    id: uuid().primaryKey().defaultRandom(),
+    title: varchar({length: 80}).notNull(),
+    body: text().notNull(),
+    orgId: text().notNull(),
+    createdAt: timestamp().defaultNow(),
+    updatedAt: timestamp().defaultNow(),
+    // userId: uuid().references(() => userTable.id), 
+})
+
+// export const userTable = pgTable('users',{
+//     id: uuid().primaryKey().defaultRandom(),
+//     name: varchar({length: 80}).notNull(),
+//     email: varchar({length: 80}).notNull(),
+//     password: varchar({length: 80}).notNull(),
+//     orgId: text().notNull(),
+// })
